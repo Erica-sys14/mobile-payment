6777e05e306b58c3f566b74c43410eab03be2a1c
Erica-sys14 <ngoeocke@gmail.com>
2021-06-18 18:46:36 +0100

    m

--- lib/domains/article.dart
+++ lib/domains/article.dart
@@ -1,30 +1,20 @@
 class Article {
-  String name;
-  String description;
-  int is_rate;
-  int check_stored;
-  String create_at;
-  String update_at;
+  /*final String name;
+  final String description;
+  final int is_rate;
+  final int check_stored;
+  final String create_at;*/
+  final List <dynamic> article;
 
 
 
   Article(
-      this.name,
-      this.description,
-      this.is_rate,
-      this.check_stored,
-      this.create_at,
-      this.update_at
+      this.article
       );
 
-  factory Article.fromJson(Map<String, dynamic> responseData) {
+  factory Article.fromJson(List<dynamic> responseData) {
     return Article(
-      responseData['name'],
-      responseData['description'],
-      responseData['is_rate'],
-      responseData['check_stored'],
-      responseData['create_at'],
-      responseData['update_at'],
+      responseData
     );
   }
 
--- lib/provider/article_provider.dart
+++ lib/provider/article_provider.dart
@@ -1,19 +1,33 @@
-import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:myflutter/domains/article.dart';
 import 'package:myflutter/utility/app_url.dart';
 import 'package:myflutter/utility/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ArticleProvider with ChangeNotifier {
-  var _article;
 
-  Article get article => _article;
+class ArticleProvider extends ChangeNotifier {
+  Future <Article> list() async {
+    final prefs = await SharedPreferences.getInstance();
+    Response response = await get(
+      AppUrl.articles,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': prefs.getString("api_key")
+      },
+    );
 
-  void setArticle(Article article) {
-    _article = article;
-    notifyListeners();
+    var result;
+
+    if (response.statusCode == 200) {
+      return Article.fromJson(json.decode(response.body)['data']['data']);
+    }
+
+    return result;
   }
-
 }
+
+
+
